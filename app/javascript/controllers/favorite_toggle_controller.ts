989700d151ebda1d4719784f2cf8496d4d750524
Oktaviandi Hadi Nugraha <oktaviandi@gmail.com>
2023-03-31 15:00:21 +0700

    Hide show stimulur

---
+++ app/javascript/controllers/favorite_toggle_controller.ts
@@ -0,0 +1,35 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class FavoriteToggleController extends Controller {
+  static targets = ["elementToHide", "elementWithText"]
+  elementToHideTarget: HTMLElement
+  elementWithTextTarget: HTMLElement
+
+  static values = { visible: Boolean }
+  visibleValue: boolean
+
+  toggle(): void {
+    this.flipState()
+  }
+
+  flipState(): void {
+    this.visibleValue = !this.visibleValue
+  }
+
+  visibleValueChanged(): void {
+    this.updateHiddenClass()
+    this.updateText()
+  }
+
+  updateHiddenClass(): void {
+    this.elementToHideTarget.classList.toggle("hidden", !this.visibleValue)
+  }
+
+  newText(): string {
+    return this.visibleValue ? "Hide" : "Show"
+  }
+
+  updateText(): void {
+    this.elementWithTextTarget.innerText = this.newText()
+  }
+}
